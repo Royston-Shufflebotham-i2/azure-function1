b84ff923b2e8c0e7da0d87db2e13e362f84231ff
Royston Shufflebotham <Royston.Shufflebotham@i2group.com>
2022-04-24 00:41:31 +0100

    Switch to TS, start adding secrets + cosmos

--- src/HttpTriggered/index.ts
+++ src/HttpTriggered/index.ts
@@ -1,5 +1,26 @@
+import { CosmosClient } from "@azure/cosmos";
+
+import { DefaultAzureCredential } from "@azure/identity";
+import { SecretClient } from "@azure/keyvault-secrets";
+
+// const cosmosClient = new CosmosClient({
+// });
+
 module.exports = async function (context, req) {
   context.log("JavaScript HTTP function processed a request.");
+
+  const credential = new DefaultAzureCredential();
+
+  const vaultName = "kv-roystonapplication";
+  const secretName = "cosmos-roystonapplication-PrimaryConnectionString";
+
+  const url = `https://${vaultName}.vault.azure.net`;
+  const client = new SecretClient(url, credential);
+
+  const secret = await client.getSecret(secretName);
+  console.log("secret", secret.name);
+  console.log("properties", JSON.stringify(secret.properties));
+
   if (req.scheduleStatus) {
     context.res = {
       body: `Timer-triggered: ${JSON.stringify(req)}`,
