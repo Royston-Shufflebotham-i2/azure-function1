cac8f401acba747eee9247a240c296448d66d878
Royston Shufflebotham <Royston.Shufflebotham@i2group.com>
2022-04-24 14:55:49 +0100

    Combine scripts, compile with ncc

--- code/src/index.ts
+++ code/src/index.ts
@@ -1,14 +1,29 @@
 import { CosmosClient } from "@azure/cosmos";
-import type { Context } from "@azure/functions";
+import type { Context, HttpRequest, Timer } from "@azure/functions";
 import { DefaultAzureCredential } from "@azure/identity";
 import { SecretClient } from "@azure/keyvault-secrets";
 
 const secretFullPath = process.env.I2_COSMOS_CONNECTION_STRING_SECRET_NAME; // keyvaultname/secretname
 const managedIdentityClientId = process.env.I2_MANAGED_IDENTITY_CLIENT_ID;
 
-module.exports = async function (context: Context, req) {
-  context.log.info("JavaScript HTTP function processed a request.");
+exports.httpTrigger = async (context: Context, req: HttpRequest) => {
+  context.log.info("HTTP trigger processing request.");
 
+  const name = req.query.name || (req.body && req.body.name);
+  const result = await handleRequest(context, name);
+
+  return {
+    status: 200,
+    body: result,
+  };
+};
+
+exports.timerTrigger = async (context: Context, timer: Timer) => {
+  context.log.info("Timer trigger processing request");
+  await handleRequest(context);
+};
+
+async function handleRequest(context: Context, name?: string) {
   process.env.AZURE_LOG_LEVEL = "verbose";
 
   const [vaultName, secretName] = secretFullPath.split("/");
@@ -54,13 +69,9 @@ module.exports = async function (context: Context, req) {
   };
   await container.items.create(item);
 
-  const name = req.query.name || (req.body && req.body.name);
   const responseMessage = name
     ? "Hello, " + name + ". This HTTP triggered function executed successfully."
     : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
 
-  return {
-    // status: 200, /* Defaults to 200 */
-    body: responseMessage,
-  };
-};
+  return responseMessage;
+}
--- src/TimerTriggered/index.ts
+++
@@ -1,4 +0,0 @@
-module.exports = async function (context, req) {
-  context.log("JavaScript Timer function processed a request.");
-  console.log("req", req);
-};
